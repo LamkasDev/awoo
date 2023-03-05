a5c1a335acc5398ad10f7b7d4c8a8dd8cf582ee9
LamkasDev <auth@lamkas.dev>
2023-03-05 09:12:13 +0100

    fix: fix sign extensions for identifiers

--- cmd/awooll/statement_compile/compile_primitive.go
+++ cmd/awooll/statement_compile/compile_primitive.go
@@ -5,7 +5,6 @@ import (
 	"github.com/LamkasDev/awoo-emu/cmd/awooll/compiler_details"
 	"github.com/LamkasDev/awoo-emu/cmd/awooll/encoder"
 	"github.com/LamkasDev/awoo-emu/cmd/awooll/node"
-	"github.com/LamkasDev/awoo-emu/cmd/awoomu/cpu"
 	"github.com/LamkasDev/awoo-emu/cmd/common/arch"
 	"github.com/LamkasDev/awoo-emu/cmd/common/instruction"
 )
@@ -13,22 +12,33 @@ import (
 func CompileNodePrimitive(ccompiler *compiler.AwooCompiler, n node.AwooParserNode, d []byte, details *compiler_details.CompileNodeValueDetails) ([]byte, error) {
 	primitiveValue := node.GetNodePrimitiveValue(&n).(int64)
 	if primitiveValue > arch.AwooImmediateSmallMax {
-		// TODO: this will get fucked by sign extension.
-		nextRegister := cpu.GetNextTemporaryRegister(details.Register)
+		luiValue := (primitiveValue >> 12) << 12
+		addiValue := primitiveValue - luiValue
+		if addiValue&0b1000_0000_0000 > 0 {
+			luiValue += addiValue * 2
+		}
+
+		// Load upper 20-bits, if primitive value is over 12-bits
 		d, err := encoder.Encode(encoder.AwooEncodedInstruction{
 			Instruction: instruction.AwooInstructionLUI,
-			Immediate:   uint32(primitiveValue),
-			Destination: nextRegister,
+			Immediate:   uint32(luiValue),
+			Destination: details.Register,
 		}, d)
 		if err != nil {
 			return d, err
 		}
-		return encoder.Encode(encoder.AwooEncodedInstruction{
-			Instruction: instruction.AwooInstructionADDI,
-			Immediate:   uint32(primitiveValue),
-			SourceOne:   nextRegister,
-			Destination: details.Register,
-		}, d)
+
+		if addiValue != 0 {
+			// Load lower 12-bits
+			return encoder.Encode(encoder.AwooEncodedInstruction{
+				Instruction: instruction.AwooInstructionADDI,
+				Immediate:   uint32(addiValue),
+				SourceOne:   details.Register,
+				Destination: details.Register,
+			}, d)
+		}
+
+		return d, nil
 	}
 
 	return encoder.Encode(encoder.AwooEncodedInstruction{
